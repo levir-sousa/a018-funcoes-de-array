00fbbb6842e5698114d94af4d0981615f7b833e3
levir-sousa <levysouza2014@gmail.com>
2022-08-25 18:21:06 -0300

    exercicio 01

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,28 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+//1
+function caixaAlta(object) {
+    const letraGrande = {...object, nome: object.nome.toUpperCase(), profissao: object.profissao.toUpperCase(), username: object.username.toUpperCase(), senha: object.senha.toUpperCase()}
+    
+    return letraGrande
+}
+// console.log(caixaAlta(objeto));
+
+//2
+function textoCorrrido(objeto) {
+    const objetoNovo={...objeto, textoCorrrido: objeto.nome + objeto.profissao + objeto.username + objeto.senha}
+    return objetoNovo.textoCorrrido
+}
+// console.log(textoCorrrido(objeto));
+
+//3
+function imprimir(objeto, callback) {
+    const imprimir = callback(objeto)
+
+    return console.log(imprimir);
+}
+
+imprimir(objeto, caixaAlta)
+imprimir(objeto, textoCorrrido)
