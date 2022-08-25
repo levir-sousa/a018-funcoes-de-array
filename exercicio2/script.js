ec8cc8007a1f162c6637eb0c487ecbcc528a3556
levir-sousa <levysouza2014@gmail.com>
2022-08-25 18:21:16 -0300

    exercicio 02

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const num = [1,2,3,4,5,6,7,8]
+
+const triplos = num.map((array) => {
+    return array*3
+})
+
+console.log(triplos);
+
+const metades = num.map((array) => {
+    return array/2
+})
+console.log(metades);
